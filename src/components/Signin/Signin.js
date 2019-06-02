5b0fe64daa1bc8241b9db1bc5d4ccc6077053a32
Sarge <35126840+sargef@users.noreply.github.com>
2019-06-03 07:26:35 +0930

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://damp-eyrie-95004.herokuapp.com//signin', {
+    fetch('/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
