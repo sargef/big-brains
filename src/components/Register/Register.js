97a921a22bcfaa81c3947299e2c6c582cf1bc139
Sarge <35126840+sargef@users.noreply.github.com>
2019-06-02 19:42:16 +0930

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://git.heroku.com/damp-eyrie-95004.git/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
