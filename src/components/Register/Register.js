c1bb371772218f4b4a0fb7663c107eaf31ad6f78
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-06-08 07:39:59 +0930

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('/register', {
+    fetch('https://bigbrainapp.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
