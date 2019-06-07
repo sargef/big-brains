feb808535198159f3550b29ef5d85d5c00cb4062
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-06-08 07:59:13 +0930

    Update App.js

--- src/App.js
+++ src/App.js
@@ -121,25 +121,26 @@ const particlesOptions = {
 }
 
 const initialState = {
-    input: '',
-    imageUrl: '',
-    box: {},
-    route: 'signin',
-    isSignedIn: false,
-    user: {
-      id: '',
-      name: '',
-      email: '',
-      entries: 0,
-      joined: ''
-    }
-  }
-class App extends Component {
-  constructor() {
-    super();
-    this.state = initialState;
+  input:"",
+  imageUrl:"",
+  box: {},
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email:'',
+    entries: 0,
+    joined: ''
   }
 }
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = initialState
+  }
+
   loadUser = (data) => {
     this.setState({user: {
       id: data.id,
@@ -152,56 +153,58 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const image=document.getElementById('inputimage')
     const width = Number(image.width);
-    const height = Number(image.height);
+    const height = Number(image.height)
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
+
     }
   }
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
+  displayFaceBox= (box) => {
+    this.setState({box:box})
   }
 
-  onInputChange = (event) => {
-    this.setState({input: event.target.value});
+  onInputChange =(event) => {
+    this.setState({input: event.target.value})
   }
 
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
+  onButtonSubmit =() => {
+    this.setState({imageUrl: this.state.input})
     fetch('https://bigbrainapp.herokuapp.com/imageurl', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        input: this.state.input
-      })
+        method: 'post',
+        headers: {'Content-type': 'application/json'},
+        body:JSON.stringify({
+          input: this.state.input
+        })
     })
-      .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch('https://bigbrainapp.herokuapp.com/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
+    .then(response => response.json())
+    .then(response => {
+      if(response) {
+        fetch('https://bigbrainapp.herokuapp.com/image', {
+          method: 'put',
+          headers: {'Content-type': 'application/json'},
+          body:JSON.stringify({
+          id: this.state.user.id
           })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+      this.displayFaceBox(this.calculateFaceLocation(response))
       }
     })
-      .catch(err => console.log(err));
+    .catch(err => console.log(err));
   }
 
-  onRouteChange = (route) => {
+  onRouteChange =(route) => {
     if (route === 'signout') {
+      
       this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
@@ -210,31 +213,24 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    const {imageUrl, isSignedIn, box, route} = this.state;
     return (
       <div className="App">
-         <Particles className='particles'
-          params={particlesOptions}
+        <Particles className="particles" 
+          params={particleOptions}
         />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        { route === 'home'
-          ? <div>
-              <Logo />
-              <Rank
-                name={this.state.user.name}
-                entries={this.state.user.entries}
-              />
-              <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
-            </div>
-          : (
-             route === 'signin'
-             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            )
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {route === 'home'
+        ? <div>
+            <Logo />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
+          </div> 
+        : (route === "signin"
+          ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )
         }
       </div>
     );
