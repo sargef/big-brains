50aed64ae242efd25ef28fff3e44912b614be247
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2019-06-08 11:17:31 +0930

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://bigbrainapp.herokuapp.com/signin', {
+    fetch('https://bigbrains101back.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
